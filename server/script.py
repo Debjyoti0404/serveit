c2a85c85c7b4a584db5b35baf2e1e80e189975a9
Debjyoti0404 <debsen2003@gmail.com>
2024-07-30 16:04:55 +0530

    added requirements

--- server/script.py
+++ server/script.py
@@ -1,7 +1,7 @@
 import subprocess
 
 def deploy(cont_name: str) -> list:
-    process = subprocess.Popen(["echo", "hello"], stdout=subprocess.PIPE)
+    process = subprocess.Popen(["sudo", "echo", "hello"], stdout=subprocess.PIPE)
     data = process.communicate()
 
     return [data, cont_name]
