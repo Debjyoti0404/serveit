3c3da19933ea88b457d615534494424c96d0e29a
Debjyoti0404 <debsen2003@gmail.com>
2024-07-30 23:19:10 +0530

    added the script file

---
+++ server/setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Define the file path
+FILE="./.server_env"
+
+# Check if the file exists
+if [ -e "$FILE" ]; then
+	source "./.server_env/bin/activate"
+else
+	python3 -m venv .server_env
+fi
+
+pip install -r requirements.txt
+
+sudo fastapi dev main.py
