53d99e72194de3b8fb3f83ede2f29007a507cbb6
Debjyoti0404 <debsen2003@gmail.com>
2024-08-02 04:35:34 +0530

    temporary changes in script

--- server/script.py
+++ server/script.py
@@ -1,8 +1,8 @@
 import subprocess
 
 def deploy(cont_name: str) -> list:
-    with open('/etc/cloudflared/config.yml', 'r', encoding='utf-8') as file: 
-	data = file.readlines() 
+    #with open('/etc/cloudflared/config.yml', 'r', encoding='utf-8') as file: 
+    #data = file.readlines() 
 
     #print(data) 
     #data[1] = "Here is my modified Line 2\n"
@@ -10,8 +10,8 @@ def deploy(cont_name: str) -> list:
     #with open('example.txt', 'w', encoding='utf-8') as file: 
     #file.writelines(data) 
 
-    process = subprocess.Popen(["sudo", "restart", "cloudflared"], stdout=subprocess.PIPE)
+    process = subprocess.Popen(["sudo", "echo", "hello"], stdout=subprocess.PIPE)
     output = process.communicate()
 
-    return [data, cont_name]
+    return [output, cont_name]
 
