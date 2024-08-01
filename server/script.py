fff3179b10e8cf08c75028f9049ed6aa69ca2981
Debjyoti0404 <debsen2003@gmail.com>
2024-08-02 04:13:24 +0530

    checking if the script is working or not

--- server/script.py
+++ server/script.py
@@ -1,8 +1,17 @@
 import subprocess
 
 def deploy(cont_name: str) -> list:
-    process = subprocess.Popen(["sudo", "echo", "hello"], stdout=subprocess.PIPE)
-    data = process.communicate()
+    with open('/etc/cloudflared/config.yml', 'r', encoding='utf-8') as file: 
+	data = file.readlines() 
+
+    #print(data) 
+    #data[1] = "Here is my modified Line 2\n"
+
+    #with open('example.txt', 'w', encoding='utf-8') as file: 
+    #file.writelines(data) 
+
+    process = subprocess.Popen(["sudo", "restart", "cloudflared"], stdout=subprocess.PIPE)
+    output = process.communicate()
 
     return [data, cont_name]
 
