49df8099979d40ae743fc03a30942f68ced78c8b
Debjyoti0404 <debsen2003@gmail.com>
2024-08-26 02:52:28 +0530

    update on crud

--- server/crud.py
+++ server/crud.py
@@ -3,20 +3,20 @@ from sqlalchemy.orm import Session
 from db import get_db
 from models import Item
 
-async def create_item(name: str, id: str, port: int, db: Session = Depends(get_db)):
+def create_item(name: str, id: str, port: int, db: Session = Depends(get_db)):
     item = Item(assigned_name=name, container_id=id, assigned_port=port)
     db.add(item)
     db.commit()
     db.refresh(item)
     return item
 
-async def read_item(name: str, db: Session = Depends(get_db)):
+def read_item(name: str, db: Session = Depends(get_db)):
     item = db.query(Item).filter(Item.assigned_name == name).first()
     if item is None:
         return None
     return item
 
-async def delete_item(name: str, db: Session = Depends(get_db)):
+def delete_item(name: str, db: Session = Depends(get_db)):
     item = db.query(Item).filter(Item.assigned_name == name).first()
     if item is None:
         return False
